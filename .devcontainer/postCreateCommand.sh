30f02f6fe483b3f5b375a5adece9108a9573271f
Mike Petersen <mike.petersen@loft.sh>
2023-08-28 11:51:47 -0700

    testing

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,1 +1,5 @@
+curl -L -o kind "https://kind.sigs.k8s.io/dl/v0.20.0/kind-linux-${TARGETARCH}" && install -c -m 0755 kind /usr/local/bin
+
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/${TARGETARCH}/kubectl && chmod +x ./kubectl && mv ./kubectl /usr/local/bin
+
 kind create cluster || kind get kubeconfig > $HOME/.kube/config
